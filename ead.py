81fc97cb34e1bf068e05c54fae9d45006668fb6e
Arpana-Karki <arpanakarki561@gmail.com>
2025-05-19 04:44:01 +0545

    file added

---
+++ ead.py
@@ -0,0 +1,14 @@
+print("Hello! It's me Arpana Karki.")
+
+print("     / |")
+print("    /  |")
+print("   /   |")
+print("  /    |")
+print(" /     |")
+print(" -------")
+ 
+character_name ="John"
+character_age = 28
+print("My name is " + character_name +".")
+print(character_name + " is a software engineer.")
+print(character_name + " is " + str(character_age) + " years old.") 
